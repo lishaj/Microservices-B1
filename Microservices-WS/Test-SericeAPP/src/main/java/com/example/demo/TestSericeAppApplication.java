fdad702cb5d6be793d8ce84b79d4d958c87f3ae9
ctsjava <ctsgit@gmail.com>
2020-08-12 16:13:21 +0530

    admin-server

---
+++ Microservices-WS/Test-SericeAPP/src/main/java/com/example/demo/TestSericeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSericeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSericeAppApplication.class, args);
+	}
+
+}
