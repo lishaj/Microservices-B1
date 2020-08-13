42ddfad8c857fcd995e5bf0b81462804228ada75
richardsforu <praveen.somireddy@gmail.com>
2020-08-13 16:36:57 +0530

    config-server

---
+++ Config-Server-WS/Dummy/src/main/java/com/cts/config/DummyApplication.java
@@ -0,0 +1,13 @@
+package com.cts.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyApplication.class, args);
+	}
+
+}
